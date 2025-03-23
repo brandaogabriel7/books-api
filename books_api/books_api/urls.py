ca378c9074e341be98bbc018343b7ec22cf1a8f4
Gabriel Brandão <contato@gabrielbrandao.net>
2025-03-23 19:56:19 -0300

    feat: set up static files in production

--- books_api/books_api/urls.py
+++ books_api/books_api/urls.py
@@ -16,12 +16,19 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.views.static import serve
 from django.conf import settings
 from django.urls import include
 
 urlpatterns = [
     path("api/books/", include("books.application.api.urls")),
+    re_path(
+        r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}
+    ),
+    re_path(
+        r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}
+    ),
 ]
 
 if settings.DEBUG:

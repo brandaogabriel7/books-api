cea98a27f694d54bd9e74942110842dce51c9b01
Gabriel Brandão <contato@gabrielbrandao.net>
2025-03-23 16:23:18 -0300

    test: mock enrichment service in integration tests

--- books_api/books/application/api/tests/conftest.py
+++ books_api/books/application/api/tests/conftest.py
@@ -2,10 +2,19 @@ import pytest
 from rest_framework.test import APIClient
 from django.urls import reverse
 
+from unittest.mock import patch
+
 
 @pytest.fixture
 def book_api_fixture(persisted_books_fixture: dict):
-    return {
-        "client": APIClient(),
-        "persisted_books": persisted_books_fixture["persisted_books"],
-    }
+    with patch(
+        "books.infrastructure.open_library.open_library_client.OpenLibraryClient.get_book_info"
+    ) as mock_get_book_info, patch(
+        "books.infrastructure.open_library.open_library_client.OpenLibraryClient.get_book_work"
+    ) as mock_get_book_work:
+        mock_get_book_info.return_value = None
+        mock_get_book_work.return_value = None
+        yield {
+            "client": APIClient(),
+            "persisted_books": persisted_books_fixture["persisted_books"],
+        }
